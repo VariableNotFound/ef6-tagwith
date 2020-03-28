113522c84e2116ff18434930671a048461c71ee3
joseaguilar <josemariaaguilar@gmail.com>
2020-03-28 14:13:39 +0100

    Migration to netstandard library

--- src/EF6.TagWith/Properties/AssemblyInfo.cs
+++ src/EF6.TagWith/Properties/AssemblyInfo.cs
@@ -1,37 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("EF6.TagWith")]
-[assembly: AssemblyDescription("Tag Entity Framework 6 LINQ queries")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Variable not found")]
-[assembly: AssemblyProduct("EF6.TagWith")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("EF6.TagWith.Tests")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4d87e996-f2c5-46dd-908b-960097a9fa4d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
